34bcfa0626b2f4213768307490237453221b7c4a
Aaron Brooks <pleaseshutupaaron@gmail.com>
2019-04-09 21:26:43 -0400

    Initial commit of wireframe

---
+++ src/components/headerBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class HeaderBar extends Component {
+	render() {
+		return (
+			<div class="header-bar"><p class="header-text">Good Sesh</p></div>
+		);
+	}
+}
---
+++ src/components/navBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component {
+	render() {
+		return (
+			<div class="nav-bar">
+				<div class="nav-icon-box">
+					<p class="nav-icon">SB</p>
+				</div>
+				<div class="nav-icon-box">
+					<p class="nav-icon">SB</p>
+				</div>
+				<div class="nav-icon-box">
+					<p class="nav-icon">SB</p>
+				</div>
+				<div class="nav-icon-box">
+					<p class="nav-icon">SB</p>
+				</div>
+			</div>
+		);
+	}
+}
